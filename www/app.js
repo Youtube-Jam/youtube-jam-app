c467b5b9c3b3867a97f42109dd0c7459176f78e5
João Vitor <jvpsousa@gmail.com>
2016-02-19 04:02:26 -0200

    Alterações de template e dados de vídeo na listagem principal do app

--- www/app.js
+++ www/app.js
@@ -2,6 +2,42 @@ var app = angular.module('app', ['ionic']);
 
 app.controller('MainController', function($scope) {
 	
+	$scope.playlist = [];
+	var nowPlaying = null;
+
 	$scope.title = 'Youtube Jam';
+
+	var videosRef = new Firebase('https://youtubejam.firebaseio.com/');
+
+	videosRef.on('value', function(dataSnapshot) {
+		console.log('here');
+		var data = dataSnapshot.val();
+		$scope.playlist = [];
+		$scope.nowPlaying = null;
+
+		for(var key in data) {
+			
+			console.log(data[key]);
+			var snapshot = data[key];
+
+			var video = {
+				key: key,
+				yid: snapshot.id,
+				info: snapshot.info
+			};
+
+			if(snapshot.status == -1) {
+				console.log('add to playlist');
+				$scope.playlist.push(video);
+				$scope.$apply();
+			}else if(snapshot.status == 0) {
+				console.log('add to nowPlaying');
+				$scope.nowPlaying = video;
+				$scope.$apply();
+			}
+		
+		}
+
+	});
 	
 });
